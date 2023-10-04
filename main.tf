9108b7bb6586e866e89bc1cd89d496c6c9a504b5
James Williams <james.b.williams@capgemini.com>
2023-10-04 07:11:35 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "eng2023-tf-state"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  default_tags {
+    tags = {
+      Environment = "eng-flux-demo-flux"
+    }
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.100.0/22"
+
+  tags = {
+    Name = "main"
+  }
+}
